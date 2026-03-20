ee289dac49311d2b7d90479fad1e2e55a63005ff
unknown <igl.myrick@gmail.com>
2026-03-20 11:30:42 -0700

    move Header component into QuizControl

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./Header";
 import QuizControl from "./QuizControl";
 
 function App() {
   return (
     <React.Fragment>
-      <Header/>
       <QuizControl/>
     </React.Fragment>
   );
