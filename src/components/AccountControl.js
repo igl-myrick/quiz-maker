75763b66e8a78946f0304767937c688731401740
unknown <igl.myrick@gmail.com>
2026-03-26 16:00:16 -0700

    add wip AccountControl component

---
+++ src/components/AccountControl.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Register from "./Register.js";
+import Login from "./Login.js";
+
+function AccountControl() {
+  let currentlyVisibleState = null;
+
+  return (
+    <React.Fragment>
+      {currentlyVisibleState}
+    </React.Fragment>
+  );
+}
+
+export default QuizControl;
