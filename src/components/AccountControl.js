65067f633d7820424463fa827c713dc3b1151abf
unknown <igl.myrick@gmail.com>
2026-03-30 16:01:17 -0700

    add state variables and conditional rendering branches

--- src/components/AccountControl.js
+++ src/components/AccountControl.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ManageAccount from "./ManageAccount.js";
 import Register from "./Register.js";
 import Login from "./Login.js";
+import UserHistory from "./UserHistory.js";
 
 function AccountControl() {
+  const [mainPageVisible, setMainPageVisible] = useState(false);
+  const [registerVisible, setRegisterVisible] = useState(false);
+  const [loginVisible, setLoginVisible] = useState(false);
+  const [historyVisible, setHistoryVisible] = useState(false);
+
   let currentlyVisibleState = null;
+
+  if (registerVisible) {
+    currentlyVisibleState = <Register/>;
+  } else if (loginVisible) {
+    currentlyVisibleState = <Login/>;
+  } else if (historyVisible) {
+    currentlyVisibleState = <UserHistory/>;
+  } else {
+    currentlyVisibleState = <ManageAccount/>;
+  }
 
   return (
     <React.Fragment>
