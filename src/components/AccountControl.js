dc01027030e97f5fa1b721c16b4d2d1d47da0672
unknown <igl.myrick@gmail.com>
2026-04-01 16:01:54 -0700

    move ManageAccount logic into AccountControl and add logout logic

--- src/components/AccountControl.js
+++ src/components/AccountControl.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
-import ManageAccount from "./ManageAccount.js";
+import React, { useState, useEffect } from "react";
 import Register from "./Register.js";
 import Login from "./Login.js";
 import UserHistory from "./UserHistory.js";
+import { auth } from "../firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 function AccountControl() {
   const [registerVisible, setRegisterVisible] = useState(false);
   const [loginVisible, setLoginVisible] = useState(false);
   const [historyVisible, setHistoryVisible] = useState(false);
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+
+    return () => {
+      listen();
+    }
+  }, []);
+
+  const userLogOut = () => {
+    signOut(auth);
+  }
 
   const showRegister = () => {
     setRegisterVisible(true);
@@ -41,13 +61,20 @@ function AccountControl() {
     currentlyVisibleState = <Login hideLogin={hideLogin}/>;
   } else if (historyVisible) {
     currentlyVisibleState = <UserHistory hideHistory={hideHistory}/>;
+  } else if (authUser) {
+    <div>
+      <h4>Manage Accounts</h4>
+      <p>Welcome Back, {authUser.email}</p>
+      <button type="button" onClick={userLogOut}>Log Out</button>
+    </div>
   } else {
     currentlyVisibleState = 
-      <ManageAccount
-        showRegister={showRegister}
-        showLogin={showLogin}
-        showHistory={showHistory}
-      />;
+      <div>
+        <h4>Manage Account</h4>
+        <button type="button" onClick={showLogin}>Login</button>
+        <button type="button" onClick={showRegister}>Register</button>
+        <button type="button" onClick={showHistory}>Quiz History</button>
+      </div>
   }
 
   return (
