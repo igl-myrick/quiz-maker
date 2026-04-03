a1d76158700b3f83f7d4de69fcf06840f7aa948a
unknown <igl.myrick@gmail.com>
2026-04-03 14:38:21 -0700

    add state branching logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import QuizControl from "./QuizControl";
+import AccountControl from "./AccountControl";
+import Header from "./Header";
 
 function App() {
+  const [quizControlVisible, setQuizControlVisible] = useState(false);
+
+  const handleAcctCtrlClicked = () => {
+    if (quizControlVisible) {
+      setQuizControlVisible(false);
+      document.getElementById("account-button").toggleAttribute("disabled");
+      document.getElementById("quiz-button").toggleAttribute("disabled");
+    }
+  }
+
+  const handleQuizCtrlClicked = () => {
+    if (quizControlVisible === false) {
+      setQuizControlVisible(true);
+      document.getElementById("account-button").toggleAttribute("disabled");
+      document.getElementById("quiz-button").toggleAttribute("disabled");
+    }
+  }
+
+  let currentlyVisibleState = null;
+
+  if (quizControlVisible) {
+    currentlyVisibleState = <QuizControl/>;
+  } else {
+    currentlyVisibleState = <AccountControl/>;
+  }
+
   return (
     <React.Fragment>
-      <QuizControl/>
+      <Header handleQuizCtrlClicked={handleQuizCtrlClicked} handleAcctCtrlClicked={handleAcctCtrlClicked}/>
+      {currentlyVisibleState}
     </React.Fragment>
   );
 }
