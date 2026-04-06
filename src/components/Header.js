f0565945f39b621d45b1d902f76fbaae5caa7141
unknown <igl.myrick@gmail.com>
2026-04-06 11:14:35 -0700

    refactor state buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function Header() {
+function Header(props) {
+  const { toggleControlState, buttonText } = props;
+
   return (
     <React.Fragment>
       <h1>Quiz Maker</h1>
+      <button type="button" id="quiz-button" onClick={toggleControlState}>{buttonText}</button>
     </React.Fragment>
   );
 }
