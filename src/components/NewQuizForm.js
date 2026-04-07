6435b8d23e49d8e15701d54f531e94070df3443c
unknown <igl.myrick@gmail.com>
2026-04-07 15:59:54 -0700

    update populateForm function and add state control

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -1,28 +1,46 @@
 import React from "react";
 
 function NewQuizForm() {
+  const [setupDivVisible, setSetupDivVisible] = useState(true);
+
+  let currentlyVisibleState = null;
+
+  if (setupDivVisible) {
+    currentlyVisibleState = 
+      <div id="select-quantity">
+          <p>Before we get started, how many questions should your quiz have?</p>
+          <form onSubmit={populateForm}>
+            <input id="number" type="number" max={15}/>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+  } else {
+    currentlyVisibleState = <div id="questions"></div>
+  }
+
   const populateForm = (amountOfQuestions) => {
     const formWrapper = document.getElementById("questions");
-    for (i = 0; i < amountOfQuestions; i++) {
+    const form = document.createElement("form");
+    for (i = 1; i < amountOfQuestions + 1; i++) {
       const question =
         <div>
-          
+          <label htmlFor={`question${i}`}>Question {i}</label><br/>
+          <input type="text" name={`question${i}`}/>
         </div>
+      form.append(question);
     }
+    const submitBtn = <button type="submit">Create Quiz</button>
+    form.append(submitBtn);
+    formWrapper.append(form);
+    setSetupDivVisible(false);
   }
 
   return (
     <React.Fragment>
       <h4>Create a New Quiz</h4>
-      <div id="select-quantity">
-        <p>Before we get started, how many questions should your quiz have?</p>
-        <form onSubmit={populateForm}>
-          <input id="number" type="number" max={15}/>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-
-      <div id="questions"></div>
+      {currentlyVisibleState}
     </React.Fragment>
   )
 }
+
+export default NewQuizForm;
