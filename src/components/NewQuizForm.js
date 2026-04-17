d51535baf95b069475f7ce5a9939410464ce0a27
unknown <igl.myrick@gmail.com>
2026-04-17 14:55:24 -0700

    update form handling

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -4,49 +4,31 @@ function NewQuizForm() {
   const [setupDivVisible, setSetupDivVisible] = useState(true);
   const [questionCount, setQuestionCount] = useState(0);
   const [title, setTitle] = useState("");
-  // const [quizData, setQuizData] = useState([]);
 
-  const handleSetup = (quizAmt, title) => {
-    setQuestionCount(quizAmt);
-    setTitle(title);
+  const handleSetupSubmission = (event) => {
+    event.preventDefault();
+    setQuestionCount(event.target.number.value);
+    setTitle(event.target.title.value);
     setSetupDivVisible(false);
-  }
-
-  const handleFormDataCreation = () => {
-    const inputsArray = document.getElementsByClassName("question-input");
-    console.log(inputsArray);
   }
 
   let currentlyVisibleState = null;
 
   if (setupDivVisible) {
-    currentlyVisibleState =
-      <form onSubmit={handleSetup}>
+    currentlyVisibleState = 
+      <form onSubmit={handleSetupSubmission}>
         <p>Before we get started, how many questions should your quiz have?</p>
-        <input id="number" type="number" max={15}/>
+        <input id="number" type="number" max={15} required/>
         <p>Next, what should the title of your quiz be?</p>
-        <input id="title" type="text" maxLength={60}/>
+        <input id="title" type="text" maxLength={60} required/>
         <button type="submit">Submit</button>
       </form>
   } else {
-    const form = document.createElement("form");
-    form.setAttribute("onSubmit", handleFormDataCreation);
-    const titleP = document.createElement("p");
-    titleP.innerText = title;
-
-    for (let i = 1; i < questionCount + 1; i++) {
-      const question =
-        <div>
-          <label htmlFor={`question${i}`}>Question {i}</label><br/>
-          <input className="question-input" type="text" name={`question${i}`}/>
-        </div>
-      form.append(question);
-    }
-
-    const submitBtn = <button type="submit">Submit</button>
-    form.append(submitBtn);
-
-    currentlyVisibleState = form;
+    currentlyVisibleState =
+      <div>
+        {title}
+        <br/>
+      </div>
   }
 
   return (
