2655e20aa4c105aa979cadcbf31183e6a79ef4aa
unknown <igl.myrick@gmail.com>
2026-04-23 15:59:15 -0700

    add state variables and branching logic

--- src/components/QuizControl.js
+++ src/components/QuizControl.js
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
 import NewQuizForm from "./NewQuizForm";
+import QuizList from "./QuizList";
 
 function QuizControl() {
-  const [newFormVisible, setNewFormVisible] = useState(true);
+  const [newFormVisible, setNewFormVisible] = useState(false);
+  const [mainQuizList, setMainQuizList] = useState([]);
+
+  const handleClick = () => {
+    setNewFormVisible(!newFormVisible);
+  }
+
+  const getQuizData = (newQuiz) => {
+    const newMainQuizList = mainQuizList.concat(newQuiz);
+    setMainQuizList(newMainQuizList);
+    setNewFormVisible(false);
+  }
 
   let currentlyVisibleState = null;
+  let buttonText = null;
 
   if (newFormVisible) {
     currentlyVisibleState = <NewQuizForm getQuizData={getQuizData}/>
+    buttonText = "Back to Quiz List";
   } else {
-    currentlyVisibleState = 
-    <div>
-      
-    </div>
+    currentlyVisibleState = <QuizList quizList={mainQuizList}/>
+    buttonText = "Add a Quiz";
   }
 
   return (
     <React.Fragment>
       {currentlyVisibleState}
+      <br/>
+      <button onClick={handleClick}>{buttonText}</button>
     </React.Fragment>
   );
 }
