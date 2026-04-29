6eefe8f97bfe4cc20861764bb9ed176053898f79
unknown <igl.myrick@gmail.com>
2026-04-29 16:00:01 -0700

    add quiz view logic

--- src/components/QuizControl.js
+++ src/components/QuizControl.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import NewQuizForm from "./NewQuizForm";
 import QuizList from "./QuizList";
+import QuizView from "./QuizView";
 
 function QuizControl() {
   const [newFormVisible, setNewFormVisible] = useState(false);
   const [mainQuizList, setMainQuizList] = useState([]);
+  const [selectedQuiz, setSelectedQuiz] = useState(null);
 
   const handleClick = () => {
-    setNewFormVisible(!newFormVisible);
+    if (selectedQuiz !== null) {
+      setNewFormVisible(false);
+      setSelectedQuiz(null);
+    } else {
+      setNewFormVisible(!newFormVisible);
+    }
   }
 
   const getQuizData = (newQuiz) => {
@@ -16,14 +23,22 @@ function QuizControl() {
     setNewFormVisible(false);
   }
 
+  const handleChangingSelectedQuiz = (id) => {
+    const selection = mainQuizList.find(quiz => quiz.id === id);
+    setSelectedQuiz(selection);
+  }
+
   let currentlyVisibleState = null;
   let buttonText = null;
 
-  if (newFormVisible) {
+  if (selectedQuiz !== null) {
+    currentlyVisibleState = <QuizView quiz={selectedQuiz}/>
+    buttonText = "Back to Quiz List";
+  } else if (newFormVisible) {
     currentlyVisibleState = <NewQuizForm getQuizData={getQuizData}/>
     buttonText = "Back to Quiz List";
   } else {
-    currentlyVisibleState = <QuizList quizList={mainQuizList}/>
+    currentlyVisibleState = <QuizList quizList={mainQuizList} onPreviewClicked={handleChangingSelectedQuiz}/>
     buttonText = "Add a Quiz";
   }
 
