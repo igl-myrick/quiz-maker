42130921ca36dd15c569420a0d0b16b173cec0e9
unknown <igl.myrick@gmail.com>
2026-05-01 11:23:38 -0700

    add dynamic form population

--- src/components/QuizView.js
+++ src/components/QuizView.js
@@ -1,16 +1,34 @@
 import React from "react";
 
 function QuizView(props) {
-  const {title, formData, id} = props;
+  const { quiz } = props;
 
-  const parseFormData = () => {
-    console.log(formData);
+  const populateForm = () => {
+    let formArr = [];
+    for (let i = 0; i < Object.values(quiz.questionList).length; i++) {
+      const elem =
+        <div key={i}>
+          <label htmlFor={`question${i}`}>{Object.values(quiz.questionList)[i]}</label>
+          <br/>
+          <input
+            id={`question${i}`}
+            type="text"
+            maxLength={60}/>
+        </div>
+      formArr.push(elem);
+    }
+    return formArr;
   }
+
+  const elems = populateForm();
 
   return (
     <React.Fragment>
-      <h4>{title}</h4>
-      <button onClick={parseFormData}>Click</button>
+      <h4>{quiz.title}</h4>
+      <form>
+        {elems}
+        <button type="submit">Submit</button>
+      </form>
     </React.Fragment>
   );
 }
