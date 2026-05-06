3992b2af6f864abcdf8860c69e94eb209fc2e8d2
unknown <igl.myrick@gmail.com>
2026-05-06 16:01:38 -0700

    add results color styling

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -1,20 +1,33 @@
 import React from "react";
 
 function QuizResults(props) {
+  const styleCorrect = { color: "green" };
+  const styleWrong = { color: "red" };
+
+  const trimInput = (input) => {
+    return input.replace(/[.,\/\\#!$%^&*;:{}=\-_`~()@+"'<>?|]/g, "").toUpperCase();
+  }
+
   const displayResults = () => {
     const output = [];
     for (let i = 0; i < Object.entries(props.resultsData.questionList).length; i++) {
+      let elemStyle = null;
+      if (trimInput(Object.values(props.resultsData.answerList)[i]) === trimInput(Object.values(props.resultsData.userAnswers)[i])) {
+        elemStyle = styleCorrect;
+      } else {
+        elemStyle = styleWrong;
+      }
       output.push(
         <div key={i}>
           <p><strong>Question {i + 1}: {Object.values(props.resultsData.questionList)[i]}</strong></p>
           <p>Answer: {Object.values(props.resultsData.answerList)[i]}</p>
-          <p>Your answer: {Object.values(props.resultsData.userAnswers)[i]}</p>
+          <p style={elemStyle}>Your answer: {Object.values(props.resultsData.userAnswers)[i]}</p>
         </div>
       );
     }
     return output;
   }
-
+  
   const elems = displayResults();
  
   return (
