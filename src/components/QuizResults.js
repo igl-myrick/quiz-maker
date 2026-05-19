2e8af6e9857e0902d4752e47e7bca92932b151f4
unknown <igl.myrick@gmail.com>
2026-05-19 15:47:15 -0700

    update props name

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -10,18 +10,18 @@ function QuizResults(props) {
 
   const displayResults = () => {
     const output = [];
-    for (let i = 0; i < Object.entries(props.resultsData.questionList).length; i++) {
+    for (let i = 0; i < Object.entries(props.results.questionList).length; i++) {
       let elemStyle = null;
-      if (trimInput(Object.values(props.resultsData.answerList)[i]) === trimInput(Object.values(props.resultsData.userAnswers)[i])) {
+      if (trimInput(Object.values(props.results.answerList)[i]) === trimInput(Object.values(props.results.userAnswers)[i])) {
         elemStyle = styleCorrect;
       } else {
         elemStyle = styleWrong;
       }
       output.push(
         <div key={i}>
-          <p><strong>Question {i + 1}: {Object.values(props.resultsData.questionList)[i]}</strong></p>
-          <p>Answer: {Object.values(props.resultsData.answerList)[i]}</p>
-          <p style={elemStyle}>Your answer: {Object.values(props.resultsData.userAnswers)[i]}</p>
+          <p><strong>Question {i + 1}: {Object.values(props.results.questionList)[i]}</strong></p>
+          <p>Answer: {Object.values(props.results.answerList)[i]}</p>
+          <p style={elemStyle}>Your answer: {Object.values(props.results.userAnswers)[i]}</p>
         </div>
       );
     }
@@ -32,7 +32,7 @@ function QuizResults(props) {
  
   return (
     <React.Fragment>
-      <h4>{props.resultsData.title}</h4>
+      <h4>{props.results.title}</h4>
       {elems}
     </React.Fragment>
   );
