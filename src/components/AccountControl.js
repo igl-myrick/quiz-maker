94fdda995ff010bd3846fc649b742ece175b8df7
unknown <igl.myrick@gmail.com>
2026-05-21 16:01:28 -0700

    move UserHistory component to QuizControl

--- src/components/AccountControl.js
+++ src/components/AccountControl.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Register from "./Register.js";
 import Login from "./Login.js";
-import UserHistory from "./UserHistory.js";
 import { auth } from "../firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 
 function AccountControl() {
   const [registerVisible, setRegisterVisible] = useState(false);
   const [loginVisible, setLoginVisible] = useState(false);
-  const [historyVisible, setHistoryVisible] = useState(false);
   const [authUser, setAuthUser] = useState(null);
 
   useEffect(() => {
@@ -45,22 +43,12 @@ function AccountControl() {
     setLoginVisible(false);
   }
 
-  const showHistory = () => {
-    setHistoryVisible(true);
-  }
-
-  const hideHistory = () => {
-    setHistoryVisible(false);
-  }
-
   let currentlyVisibleState = null;
 
   if (registerVisible) {
     currentlyVisibleState = <Register hideRegister={hideRegister}/>;
   } else if (loginVisible) {
     currentlyVisibleState = <Login hideLogin={hideLogin}/>;
-  } else if (historyVisible) {
-    currentlyVisibleState = <UserHistory hideHistory={hideHistory}/>;
   } else if (authUser) {
     currentlyVisibleState =
       <div>
@@ -74,7 +62,6 @@ function AccountControl() {
         <h4>Manage Account</h4>
         <button type="button" onClick={showLogin}>Login</button>
         <button type="button" onClick={showRegister}>Register</button>
-        <button type="button" onClick={showHistory}>Quiz History</button>
       </div>
   }
 
