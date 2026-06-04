8c75e8d740ca395b1b8e3922fc6c5fde44385c59
unknown <igl.myrick@gmail.com>
2026-06-04 10:32:45 -0700

    add questionCount as props

--- src/components/ResultsPreview.js
+++ src/components/ResultsPreview.js
@@ -1,19 +1,17 @@
 import React from "react";
 
 function ResultsPreview(props) {
-  const { title, questionList, onPreviewClicked, id } = props;
-
-  const questionCount = Object.entries(questionList).length;
+  const { title, questionCount, onPreviewClicked, id } = props;
 
   return (
-      <React.Fragment>
-        <div>
-          <h4>{title}</h4>
-          <p>{questionCount} questions</p>
-          <button type="button" onClick={() => onPreviewClicked(id)}>View Results</button>
-        </div>
-      </React.Fragment>
-    );
+    <React.Fragment>
+      <div>
+        <h4>{title}</h4>
+        <p>{questionCount} questions</p>
+        <button type="button" onClick={() => onPreviewClicked(id)}>View Results</button>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default ResultsPreview;
