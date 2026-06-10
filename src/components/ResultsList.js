640ec2e121c774d9fff211d52525b365b6c82737
unknown <igl.myrick@gmail.com>
2026-06-10 15:27:34 -0700

    change page contents based on if user has any associated quizResults

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -2,18 +2,26 @@ import React from "react";
 import ResultsPreview from "./ResultsPreview";
 
 function ResultsList(props) {
+  let currentlyVisibleState = null;
+
+  if (Object.entries(props.resultsList).length > 0) {
+    currentlyVisibleState = props.resultsList.map((results) =>
+      <ResultsPreview
+        onPreviewClicked={props.onPreviewClicked}
+        title={results.title}
+        questionCount={results.questionCount}
+        quizId={results.quizId}
+        id={results.id}
+        key={results.id}/>
+    );
+  } else {
+    currentlyVisibleState = <p>No history yet!</p>;
+  }
+
   return (
     <React.Fragment>
       <h4>Your History</h4>
-      {props.resultsList.map((results) =>
-        <ResultsPreview
-          onPreviewClicked={props.onPreviewClicked}
-          title={results.title}
-          questionCount={results.questionCount}
-          quizId={results.quizId}
-          id={results.id}
-          key={results.id}/>
-      )}
+      {currentlyVisibleState}
     </React.Fragment>
   )
 }
