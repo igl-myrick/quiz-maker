4fd9a0f4032063a150925f9dc166022979cb0531
unknown <igl.myrick@gmail.com>
2026-06-10 11:13:46 -0700

    add sortObj function and update displayResults properties

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -8,20 +8,35 @@ function QuizResults(props) {
     return input.replace(/[.,/\\#!$%^&*;:{}=\s\-_`~()@+"'<>?|]/g, "").toUpperCase();
   }
 
+  const sortObj = (obj) => {
+    const sortedObj = Object.entries(obj).sort(([keyA], [keyB]) => 
+      keyA.localeCompare(keyB)
+    );
+    const output = [];
+    for (let i = 0; i < sortedObj.length; i++) {
+      output.push(sortedObj[i][1]);
+    }
+    return output;
+  }
+
+  const sortedQuestionList = sortObj(props.quiz.questionList);
+  const sortedAnswerList = sortObj(props.quiz.answerList);
+  const sortedUserAnswers = sortObj(props.results.userAnswers);
+
   const displayResults = () => {
     const output = [];
     for (let i = 0; i < props.results.questionCount; i++) {
       let elemStyle = null;
-      if (trimInput(Object.values(props.quiz.answerList)[i]) === trimInput(Object.values(props.results.userAnswers)[i])) {
+      if (trimInput(sortedAnswerList[i]) === trimInput(sortedUserAnswers[i])) {
         elemStyle = styleCorrect;
       } else {
         elemStyle = styleWrong;
       }
       output.push(
         <div key={i}>
-          <p><strong>Question {i + 1}: {Object.values(props.quiz.questionList)[i]}</strong></p>
-          <p>Answer: {Object.values(props.quiz.answerList)[i]}</p>
-          <p style={elemStyle}>Your answer: {Object.values(props.results.userAnswers)[i]}</p>
+          <p><strong>Question {i + 1}: {sortedQuestionList[i]}</strong></p>
+          <p>Answer: {sortedAnswerList[i]}</p>
+          <p style={elemStyle}>Your answer: {sortedUserAnswers[i]}</p>
         </div>
       );
     }
